0e159fd568f8cdebf5fbda05ff756cbd493d65f3
Amadeus <mazurenkodmitriy57@gmail.com>
2021-01-29 10:21:50 +0200

    server: sign in with token, implement require signin method

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user')
 const shortId = require('shortid')
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
 
 exports.signup = (req, res) => {
     User
@@ -28,3 +30,45 @@ exports.signup = (req, res) => {
             })
         })
 }
+
+exports.signin = (req, res) => {
+    const {email, password} = req.body
+    // check if user exist
+    User
+        .findOne({email})
+        .exec((err, user) => {
+            if (err || !user) {
+                return res.status(400).json({
+                    error: "User with this email does not exist. Please signup."
+                })
+            }
+            // authenticate
+            if(!user.authenticate(password)) {
+                return res.status(400).json({
+                    error: "Email and password do not match"
+                })
+            }
+
+            // generate a token and send to client
+            const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: '1d'})
+
+            res.cookie('designium-token', token, {expiresIn: '1d'})
+            const {_id, username, name, email, role} = user
+            return res.json({
+                token,
+                user: {_id, username, name, email, role}
+            })
+        })
+}
+
+exports.signout = (req, res) => {
+    res.clearCookie('designium-token')
+    res.json({
+        message: 'Sign Out success'
+    })
+}
+
+exports.requireSignin = expressJwt({
+    algorithms: ['HS256'],
+    secret: process.env.JWT_SECRET
+})
